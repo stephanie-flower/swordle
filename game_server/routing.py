650c75c53e8a13b789fbd5a3a19b30ad850509c9
Leo Durrant <poweredbykurisu@hotmail.com>
2022-02-05 15:22:00 +0000

    Added the rest of the app files

---
+++ game_server/routing.py
@@ -0,0 +1,8 @@
+# chat/routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/session/(?P<game_name>\w+)/$', consumers.GameSessionConsumer.as_asgi()),
+]
---
+++ game_server/urls.py
@@ -0,0 +1,9 @@
+# game_server/urls.py
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<str:game_name>/', views.game, name='game'),
+]
