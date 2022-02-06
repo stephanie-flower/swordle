7663496e0f0d908a163275ae872a091e4c88c884
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-02-06 12:19:33 +0000

    Redirect '/' to 'room/'

--- swordle/urls.py
+++ swordle/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('game_server.urls')),
     path('room/', include('game_server.urls')),
     path('admin/', admin.site.urls),
 ]
